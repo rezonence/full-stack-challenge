3a5eac5ff221b9eac08df9ed1d8f2c29c3292d4d
Daniel Jabry <djabry@gmail.com>
2022-03-16 14:10:16 +0000

    DEV-4364 Correct linting errors

--- src/PollStack.ts
+++ src/PollStack.ts
@@ -21,7 +21,7 @@ export class PollStack extends Stack {
         votes: this.polls.tables.Votes,
         polls: this.polls.tables.Polls,
         region: this.region,
-        configFileName: "config.json"
+        configFileName: 'config.json'
       })
     }
 }
