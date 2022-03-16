063cc378427379483f0a8af3c7d3a47c8c32256e
Daniel Jabry <djabry@gmail.com>
2022-03-16 14:19:51 +0000

    DEV-4364 Correct linting

--- src/site/SiteConstructOptions.ts
+++ src/site/SiteConstructOptions.ts
@@ -1,4 +1,4 @@
-import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2-alpha'
 import { IdentityPool } from '@aws-cdk/aws-cognito-identitypool-alpha'
 import { Table } from 'aws-cdk-lib/aws-dynamodb'
 
