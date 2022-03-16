67d4f96dbb548dea3fa5380fe078d25817e93d98
Daniel Jabry <djabry@gmail.com>
2022-03-16 21:13:32 +0000

    DEV-4364 Correct linting errors

--- src/poller/resolveSiteConfig.ts
+++ src/poller/resolveSiteConfig.ts
@@ -2,11 +2,11 @@ import { configFileName } from './configFilename'
 import type { SiteConfig } from './SiteConfig'
 
 export async function resolveSiteConfig (): Promise<SiteConfig> {
-  const siteConfigUrl = new URL(`/${configFileName}`, location.origin);
+  const siteConfigUrl = new URL(`/${configFileName}`, location.origin)
   const response = await fetch(siteConfigUrl.toString(), {
-    mode: "cors",
+    mode: 'cors',
     headers: {
-      'Access-Control-Allow-Origin':'*'
+      'Access-Control-Allow-Origin': '*'
     }
   })
   return response.json()
