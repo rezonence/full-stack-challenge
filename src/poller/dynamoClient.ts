319ef8ed779d4672ecdb63f0c739be9652bd753e
Daniel Jabry <djabry@gmail.com>
2022-03-16 22:30:03 +0000

    DEV-4364 Correct linting errors

--- src/poller/dynamoClient.ts
+++ src/poller/dynamoClient.ts
@@ -2,8 +2,8 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import type { CognitoIdentityCredentialProvider } from '@aws-sdk/credential-providers'
 import { DynamoDBDocumentClient, DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import { derived, type Readable } from 'svelte/store'
-import type {SiteConfig} from "./SiteConfig";
-import {siteConfig} from "./siteConfig";
+import type { SiteConfig } from './SiteConfig'
+import { siteConfig } from './siteConfig'
 import { credentialsProvider } from './credentialsProvider'
 
 export const dynamoClient = derived<[Readable<CognitoIdentityCredentialProvider>, Readable<SiteConfig>], DynamoDBDocumentClient>([credentialsProvider, siteConfig], values => DynamoDBDocument.from(new DynamoDBClient({
